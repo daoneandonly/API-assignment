22a0689d20d6517984b88e7ca5394f6c036a4ff6
Steven <stevenawhitton@gmail.com>
2022-09-27 15:35:49 +0200

    Add Search

---
+++ src/components/atoms/Search/Search.tsx
@@ -0,0 +1,32 @@
+import router from 'next/router'
+
+// Styles
+import styles from '@/styles/Search.module.scss'
+
+// Types
+import type { FormEvent } from 'react'
+
+function Search () {
+
+  
+  function handleSearch(e: FormEvent) {
+    e.preventDefault()
+    const searchValue = e.currentTarget.querySelector('input')?.value
+
+    router.push(`search?q=${searchValue}`)
+
+  }
+
+  return (
+    <form onSubmit={(e) => { handleSearch(e) }}>
+        <input 
+          className={styles.search} 
+          type="text" 
+          placeholder='search' 
+          name='search'
+        />
+      </form>
+  )
+}
+
+export default Search
